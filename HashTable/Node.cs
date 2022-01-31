747ba1dbc615d54cf05e61e32602219a0dab9fec
srikantheddula <you@example.com>
2022-01-31 20:13:27 +0530

    [srikantheddula]:adding HashTableUc1 implemented in HashTable

---
+++ HashTable/Node.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+     class Node<K,V>
+    {
+        public struct KeyValue<K,V>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+        private readonly int size;
+        //int[] arr;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public Node(int size)// Creting a constructer to intilisize the values
+        {
+            this.size = size;
+            //arr new int [size];
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int hash = key.GetHashCode();
+            int position = hash % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                    return item.Value;
+            }
+            return default(V);
+        }
+        public void Add(K key, V value) //this method for adding the elements
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+        public bool Exists(K key) //this method for checking how many same elements in linked list
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        public void Display() // this method for displying the elements in linked list
+        {
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                    foreach (var element in linkedList)
+                    {
+                        string res = element.ToString();
+                        if (res != null)
+                            Console.WriteLine(element.Key + " " + element.Value);
+                    }
+            }
+        }
+    }
+}
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -2,11 +2,26 @@
 
 namespace HashTable
 {
-    internal class Program
+     class Program
     {
+        public static void CountNumberOfOccurance(string paragraph)
+        {
+            Node<string, int> hashTabe = new Node<string, int>(6);
+            string[] words = paragraph.Split(' ');
+            foreach (string Word in words)
+            {
+                if (hashTabe.Exists(Word))
+                    hashTabe.Add(Word.ToLower(), hashTabe.Get(Word) + 1);
+                else
+                    hashTabe.Add(Word.ToLower(), 1);
+            }
+            Console.WriteLine("Displaying after add operation ");
+            hashTabe.Display();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to HashTables");
+
+            CountNumberOfOccurance("To be or not to be");
         }
     }
 }
